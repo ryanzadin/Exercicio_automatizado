20e942efb11062518c0e1632e0ff403a4e141185
Ryan Dulko <ryandulko458@gmail.com>
2023-11-30 20:59:05 -0300

    meu primero teste

--- cypress/integration/exercicio-e2e.spec.js
+++ cypress/integration/exercicio-e2e.spec.js
@@ -14,7 +14,32 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
+
+        cy.addProdutos('Livingston All-Purpose Tight', 32, 'Black', 1)
+        cy.get('.tbay-woocommerce-breadcrumb > :nth-child(1) > a').click()
+        cy.addProdutos('Zeppelin Yoga Pant', 34, 'Green', 2)
+        cy.get('.tbay-woocommerce-breadcrumb > :nth-child(1) > a').click()
+        cy.addProdutos('Kratos Gym Pant', 36, 'Blue', 1)
+        cy.get('.tbay-woocommerce-breadcrumb > :nth-child(1) > a').click()
+        cy.addProdutos('Geo Insulated Jogging Pant', 33, 'Red', 4)
+        cy.get('.woocommerce-message > .button').click()
+        cy.get('.checkout-button').click()
+        cy.get('#billing_first_name').type('Ryan')
+        cy.get('#billing_last_name').type('Moraes')
+        cy.get('#billing_company').type('EBAC')
+        cy.get('#select2-billing_country-container').click().type('Brasil').get('[aria-selected="true"]').click()
+        cy.get('#billing_address_1').clear().type('Manoel dias')
+        cy.get('#billing_address_2').clear().type('453')
+        cy.get('#billing_city').clear().type('sao paulo')
+        cy.get('#select2-billing_state-container').click().type('sao paulo').get('[aria-selected="true"]').click()
+        cy.get('#billing_postcode').clear().type('02563-010')
+        cy.get('#billing_phone').clear().type(11937812834)
+        cy.get('#billing_email').clear().type('ryanlokino@teste.com')
+        cy.get('#createaccount').click()
+        cy.get('#terms').click()
+        cy.get('#place_order').click()
+        cy.get('#account_password').type('659293132Ry;')
+        cy.get('#place_order').click()
     });
 
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,3 +31,11 @@ Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('.woocommerce-form > .button').click()
 });
 
+Cypress.Commands.add('addProdutos', (produto, tamanho, cor, quantidade) =>{
+    cy.get('.product-block')
+.contains(produto).click()
+cy.get('.button-variable-item-' + tamanho).click()
+cy.get('.button-variable-item-' + cor).click()
+cy.get('.input-text').clear().type(quantidade)
+cy.get('.single_add_to_cart_button').click()
+})
